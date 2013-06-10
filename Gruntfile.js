cfe0068642446f502a3e20e9931a52dad0663f6c
rodyhaddad <rody@rodyhaddad.com>
2013-06-10 17:58:22 -0400

    Added a Gruntfile.js

---
+++ Gruntfile.js
@@ -0,0 +1,51 @@
+module.exports = function (grunt) {
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        destName: 'objUtils',
+        LICENSE: grunt.file.read("LICENSE"),
+        concat: {
+            options: {
+                separator: '\n\n',
+                banner: '/*! <%= pkg.name %> v<%= pkg.version %> <%= grunt.template.today("dd-mm-yyyy") %> \n<%= LICENSE %>\n*/\n\n',
+            },
+            dist: {
+                src: ["src/objUtils.prefix", "src/{common,forEach,mergeObj,makeInherit,navigateObj,publishAPI}.js", "src/exportAPI.js", "src/objUtils.suffix"],
+                dest: 'dist/<%= destName %>.js'
+            }
+        },
+        uglify: {
+            options: {
+                preserveComments: 'some' //Licences
+            },
+            dist: {
+                files: {
+                    'dist/<%= destName %>.min.js': ['dist/<%= destName %>.js'],
+                }
+            }
+        },
+
+        jshint: {
+            files: ['src/*.js'],
+            options: {
+                // options here to override JSHint defaults
+                globals: {
+                    jQuery: true,
+                    module: true,
+                    document: true
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+
+    grunt.registerTask('lint', ['jshint']);
+
+    grunt.registerTask('minify', ['concat', 'uglify']);
+
+    grunt.registerTask('default', ['jshint', 'concat', 'uglify']);
+
+};
